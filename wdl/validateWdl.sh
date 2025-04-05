59870c0e985b9d25c5dc5a65cb46e36e6fae87a8
fcunial <fcunial@broadinstitute.org>
2025-04-04 20:14:36 -0400

    fixes

--- wdl/validateWdl.sh
+++ wdl/validateWdl.sh
@@ -3,6 +3,7 @@
 set -x
 WOMTOOL_PATH="/Users/fcunial/apps/cromwell/womtool-84.jar"
 
+java -jar ${WOMTOOL_PATH} validate -l Kanpig.wdl
 java -jar ${WOMTOOL_PATH} validate -l TruvariIntrasample.wdl
 java -jar ${WOMTOOL_PATH} validate -l Resolve.wdl
 java -jar ${WOMTOOL_PATH} validate -l PAV2SVs.wdl
