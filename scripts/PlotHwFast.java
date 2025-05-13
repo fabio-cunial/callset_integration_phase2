ed04ba60f1051b0546405bac12ded24191331961
fcunial <fcunial@broadinstitute.org>
2025-05-13 10:21:44 -0400

    fixes

---
+++ scripts/PlotHwFast.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.util.zip.*;
+
+
+/**
+ * 
+ */
+public class PlotHwFast {
+    
+    /**
+     * 
+     */
+    public static void main(String[] args) throws IOException {
+        final String VCF_GZ = args[0];
+        final String OUT_HWE = args[1];
+        
+        final char FIELD_SEPARATOR = '\t';
+        final char GT_SEPARATOR = ':';
+        final int QUANTUM = 100;  // Arbitrary
+        
+        boolean onLeft, onRight;
+        int i, p;
+        int length, nLines, start, end;
+        int gt00, gt01, gt11;
+        long ms;
+        String str;
+        BufferedReader br;
+        BufferedWriter bwHwe;
+        
+        br = new BufferedReader(new InputStreamReader(new GZIPInputStream(new FileInputStream(VCF_GZ))));
+        bwHwe = new BufferedWriter(new FileWriter(OUT_HWE));
+        bwHwe.write("AA,AB,BB\n");
+        str=br.readLine(); nLines=0;
+        ms=System.currentTimeMillis();
+        while (str!=null) {
+            gt00=0; gt01=0; gt11=0;
+            length=str.length();
+            start=0;
+            for (i=0; i<=8; i++) start=str.indexOf(FIELD_SEPARATOR,start)+1;
+            do {
+                end=str.indexOf(FIELD_SEPARATOR,start);
+                if (end<0) end=length;
+                p=str.indexOf(GT_SEPARATOR,start);
+                if (p==start+3) {
+                    onLeft=str.charAt(start)=='1';
+                    onRight=str.charAt(start+2)=='1';
+                    if (onLeft==onRight) {
+                        if (onLeft) gt11++;
+                        else gt00++;
+                    }
+                    else gt01++;
+                }
+                start=end+1;
+            } while (start<length);
+            if (gt00+gt01+gt11>0) bwHwe.write(gt00+","+gt01+","+gt11+"\n");
+            nLines++;
+            if (nLines%QUANTUM==0) System.err.println("Processed "+nLines+" lines in "+((double)(System.currentTimeMillis()-ms)/1000)+"s");
+            str=br.readLine();
+        }
+        br.close(); bwHwe.close();
+    }
+    
+}
