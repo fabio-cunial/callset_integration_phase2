868a03b17aa4643f023de2bbcf0fc939c78d2b2d
fcunial <fcunial@broadinstitute.org>
2025-08-18 16:11:08 -0400

    fixes

--- wdl/validateWdl.sh
+++ wdl/validateWdl.sh
@@ -3,6 +3,7 @@
 set -x
 WOMTOOL_PATH="/Users/fcunial/apps/cromwell/womtool-84.jar"
 
+java -jar ${WOMTOOL_PATH} validate -l BenchCohortTrios.wdl
 java -jar ${WOMTOOL_PATH} validate -l PlotHweFocusedAc.wdl
 java -jar ${WOMTOOL_PATH} validate -l BenchCohortSamples.wdl
 java -jar ${WOMTOOL_PATH} validate -l PlotHweFocused.wdl
