88f6b30f47fc0cce7f33dba61829cfa78516ee5d
fcunial <fcunial@broadinstitute.org>
2025-08-19 16:12:04 -0400

    fixes

--- scripts/BenchCohortTrios.m
+++ scripts/BenchCohortTrios.m
@@ -2,23 +2,30 @@ SOURCE_DIR='.';
 FONT_SIZE=18;
 DELTA=0.4;
 
-figure(1);
-
-
-
-
-A=load('trios_all.csv');
-[nrows,ncolumns]=size(A);
-% Columns of A:
+% Columns:
 % ${KANPIG_N_GOOD_ALT},${KANPIG_N_MERR},
 % ${F07_N_GOOD_ALT},${F07_N_MERR},
 % ${F09_N_GOOD_ALT},${F09_N_MERR},
 % ${COHORT_MERGED_07_N_GOOD_ALT},${COHORT_MERGED_07_N_MERR},
 % ${COHORT_MERGED_09_N_GOOD_ALT},${COHORT_MERGED_09_N_MERR},
 % ${COHORT_REGENOTYPED_07_N_GOOD_ALT},${COHORT_REGENOTYPED_07_N_MERR},
-% ${COHORT_REGENOTYPED_09_N_GOOD_ALT},${COHORT_REGENOTYPED_09_N_MERR}
+% ${COHORT_REGENOTYPED_09_N_GOOD_ALT},${COHORT_REGENOTYPED_09_N_MERR},
+% ${KANPIG_DENOVO},   // 15th column (1-based)
+% ${F07_DENOVO},
+% ${F09_DENOVO},
+% ${COHORT_MERGED_07_DENOVO},
+% ${COHORT_MERGED_09_DENOVO},
+% ${COHORT_REGENOTYPED_07_DENOVO},
+% ${COHORT_REGENOTYPED_09_DENOVO}
+
+
+
+
+A=load('trios_all.csv');
+[nrows,ncolumns]=size(A);
 
 # All, 07.
+figure(1);
 subplot(3,2,1); hold on;
 X=ones(nrows,1).*1 -DELTA/2 + rand(nrows,1).*DELTA;
 Y=A(:,2)./(A(:,2)+A(:,1));
@@ -39,6 +46,30 @@ plot(X,Y,'.');
 title('All variants, 07.'); axis([0,5,0,0.14]); axis square; grid on; 
 xticks([1:4]); xticklabels({sprintf('kanpig\nintra'),'xgb',sprintf('truvari\ninter'),sprintf('kanpig\ninter')});
 ylabel('Mendelian error rate'); set(gca,'fontsize',FONT_SIZE);
+
+figure(2);
+subplot(3,2,1); hold on;
+X=ones(nrows,1).*1 -DELTA/2 + rand(nrows,1).*DELTA;
+Y=A(:,15);
+plot(X,Y,'.');
+
+X=ones(nrows,1).*2 -DELTA/2 + rand(nrows,1).*DELTA;
+Y=A(:,16);
+plot(X,Y,'.');
+
+X=ones(nrows,1).*3 -DELTA/2 + rand(nrows,1).*DELTA;
+Y=A(:,18);
+plot(X,Y,'.');
+
+X=ones(nrows,1).*4 -DELTA/2 + rand(nrows,1).*DELTA;
+Y=A(:,20);
+plot(X,Y,'.');
+
+title('All variants, 07.'); axis([0,5,0,0.14]); axis square; grid on; 
+xticks([1:4]); xticklabels({sprintf('kanpig\nintra'),'xgb',sprintf('truvari\ninter'),sprintf('kanpig\ninter')});
+ylabel('De novo rate'); set(gca,'fontsize',FONT_SIZE);
+
+
 
 # All, 09.
 subplot(3,2,2); hold on;
@@ -61,6 +92,28 @@ plot(X,Y,'.');
 title('All variants, 09.'); axis([0,5,0,0.14]); axis square; grid on; 
 xticks([1:4]); xticklabels({sprintf('kanpig\nintra'),'xgb',sprintf('truvari\ninter'),sprintf('kanpig\ninter')});
 ylabel('Mendelian error rate'); set(gca,'fontsize',FONT_SIZE);
+
+figure(2);
+subplot(3,2,2); hold on;
+X=ones(nrows,1).*1 -DELTA/2 + rand(nrows,1).*DELTA;
+Y=A(:,15);
+plot(X,Y,'.');
+
+X=ones(nrows,1).*2 -DELTA/2 + rand(nrows,1).*DELTA;
+Y=A(:,17);
+plot(X,Y,'.');
+
+X=ones(nrows,1).*3 -DELTA/2 + rand(nrows,1).*DELTA;
+Y=A(:,19);
+plot(X,Y,'.');
+
+X=ones(nrows,1).*4 -DELTA/2 + rand(nrows,1).*DELTA;
+Y=A(:,21);
+plot(X,Y,'.');
+
+title('All variants, 09.'); axis([0,5,0,0.14]); axis square; grid on; 
+xticks([1:4]); xticklabels({sprintf('kanpig\nintra'),'xgb',sprintf('truvari\ninter'),sprintf('kanpig\ninter')});
+ylabel('De novo rate'); set(gca,'fontsize',FONT_SIZE);
 
 
 
@@ -90,6 +143,31 @@ title('Inside TRs, 07.'); axis([0,5,0,0.14]); axis square; grid on;
 xticks([1:4]); xticklabels({sprintf('kanpig\nintra'),'xgb',sprintf('truvari\ninter'),sprintf('kanpig\ninter')});
 ylabel('Mendelian error rate'); set(gca,'fontsize',FONT_SIZE);
 
+figure(2);
+subplot(3,2,3); hold on;
+X=ones(nrows,1).*1 -DELTA/2 + rand(nrows,1).*DELTA;
+Y=A(:,15);
+plot(X,Y,'.');
+
+X=ones(nrows,1).*2 -DELTA/2 + rand(nrows,1).*DELTA;
+Y=A(:,16);
+plot(X,Y,'.');
+
+X=ones(nrows,1).*3 -DELTA/2 + rand(nrows,1).*DELTA;
+Y=A(:,18);
+plot(X,Y,'.');
+
+X=ones(nrows,1).*4 -DELTA/2 + rand(nrows,1).*DELTA;
+Y=A(:,20);
+plot(X,Y,'.');
+
+title('Inside TRs, 07.'); axis([0,5,0,0.14]); axis square; grid on; 
+xticks([1:4]); xticklabels({sprintf('kanpig\nintra'),'xgb',sprintf('truvari\ninter'),sprintf('kanpig\ninter')});
+ylabel('De novo rate'); set(gca,'fontsize',FONT_SIZE);
+
+
+
+
 # Inside TRs, 09.
 subplot(3,2,4); hold on;
 X=ones(nrows,1).*1 -DELTA/2 + rand(nrows,1).*DELTA;
@@ -111,6 +189,28 @@ plot(X,Y,'.');
 title('Inside TRs, 09.'); axis([0,5,0,0.14]); axis square; grid on; 
 xticks([1:4]); xticklabels({sprintf('kanpig\nintra'),'xgb',sprintf('truvari\ninter'),sprintf('kanpig\ninter')});
 ylabel('Mendelian error rate'); set(gca,'fontsize',FONT_SIZE);
+
+figure(2);
+subplot(3,2,4); hold on;
+X=ones(nrows,1).*1 -DELTA/2 + rand(nrows,1).*DELTA;
+Y=A(:,15);
+plot(X,Y,'.');
+
+X=ones(nrows,1).*2 -DELTA/2 + rand(nrows,1).*DELTA;
+Y=A(:,17);
+plot(X,Y,'.');
+
+X=ones(nrows,1).*3 -DELTA/2 + rand(nrows,1).*DELTA;
+Y=A(:,19);
+plot(X,Y,'.');
+
+X=ones(nrows,1).*4 -DELTA/2 + rand(nrows,1).*DELTA;
+Y=A(:,21);
+plot(X,Y,'.');
+
+title('Inside TRs, 09.'); axis([0,5,0,0.14]); axis square; grid on; 
+xticks([1:4]); xticklabels({sprintf('kanpig\nintra'),'xgb',sprintf('truvari\ninter'),sprintf('kanpig\ninter')});
+ylabel('De novo rate'); set(gca,'fontsize',FONT_SIZE);
 
 
 
@@ -140,6 +240,32 @@ title('Outside TRs, 07.'); axis([0,5,0,0.14]); axis square; grid on;
 xticks([1:4]); xticklabels({sprintf('kanpig\nintra'),'xgb',sprintf('truvari\ninter'),sprintf('kanpig\ninter')});
 ylabel('Mendelian error rate'); set(gca,'fontsize',FONT_SIZE);
 
+
+figure(2);
+subplot(3,2,5); hold on;
+X=ones(nrows,1).*1 -DELTA/2 + rand(nrows,1).*DELTA;
+Y=A(:,15);
+plot(X,Y,'.');
+
+X=ones(nrows,1).*2 -DELTA/2 + rand(nrows,1).*DELTA;
+Y=A(:,16);
+plot(X,Y,'.');
+
+X=ones(nrows,1).*3 -DELTA/2 + rand(nrows,1).*DELTA;
+Y=A(:,18);
+plot(X,Y,'.');
+
+X=ones(nrows,1).*4 -DELTA/2 + rand(nrows,1).*DELTA;
+Y=A(:,20);
+plot(X,Y,'.');
+
+title('Outside TRs, 07.'); axis([0,5,0,0.14]); axis square; grid on; 
+xticks([1:4]); xticklabels({sprintf('kanpig\nintra'),'xgb',sprintf('truvari\ninter'),sprintf('kanpig\ninter')});
+ylabel('De novo rate'); set(gca,'fontsize',FONT_SIZE);
+
+
+
+
 # Outside TRs, 09.
 subplot(3,2,6); hold on;
 X=ones(nrows,1).*1 -DELTA/2 + rand(nrows,1).*DELTA;
@@ -161,3 +287,26 @@ plot(X,Y,'.');
 title('Outside TRs, 09.'); axis([0,5,0,0.14]); axis square; grid on; 
 xticks([1:4]); xticklabels({sprintf('kanpig\nintra'),'xgb',sprintf('truvari\ninter'),sprintf('kanpig\ninter')});
 ylabel('Mendelian error rate'); set(gca,'fontsize',FONT_SIZE);
+
+
+figure(2);
+subplot(3,2,6); hold on;
+X=ones(nrows,1).*1 -DELTA/2 + rand(nrows,1).*DELTA;
+Y=A(:,15);
+plot(X,Y,'.');
+
+X=ones(nrows,1).*2 -DELTA/2 + rand(nrows,1).*DELTA;
+Y=A(:,16);
+plot(X,Y,'.');
+
+X=ones(nrows,1).*3 -DELTA/2 + rand(nrows,1).*DELTA;
+Y=A(:,18);
+plot(X,Y,'.');
+
+X=ones(nrows,1).*4 -DELTA/2 + rand(nrows,1).*DELTA;
+Y=A(:,20);
+plot(X,Y,'.');
+
+title('Outside TRs, 09.'); axis([0,5,0,0.14]); axis square; grid on; 
+xticks([1:4]); xticklabels({sprintf('kanpig\nintra'),'xgb',sprintf('truvari\ninter'),sprintf('kanpig\ninter')});
+ylabel('De novo rate'); set(gca,'fontsize',FONT_SIZE);
--- scripts/BenchCohortTrios.sh
+++ scripts/BenchCohortTrios.sh
@@ -31,7 +31,15 @@ while read CHILD_ID; do
     COHORT_REGENOTYPED_09_N_GOOD_ALT=$(grep ^ngood_alt ${INPUT_DIR}/${CHILD_ID}_cohort_regenotyped_09_all.txt | cut -f 2)
     COHORT_REGENOTYPED_09_N_MERR=$(grep ^nmerr ${INPUT_DIR}/${CHILD_ID}_cohort_regenotyped_09_all.txt | cut -f 2)
     
-    echo "${KANPIG_N_GOOD_ALT},${KANPIG_N_MERR},${F07_N_GOOD_ALT},${F07_N_MERR},${F09_N_GOOD_ALT},${F09_N_MERR},${COHORT_MERGED_07_N_GOOD_ALT},${COHORT_MERGED_07_N_MERR},${COHORT_MERGED_09_N_GOOD_ALT},${COHORT_MERGED_09_N_MERR},${COHORT_REGENOTYPED_07_N_GOOD_ALT},${COHORT_REGENOTYPED_07_N_MERR},${COHORT_REGENOTYPED_09_N_GOOD_ALT},${COHORT_REGENOTYPED_09_N_MERR}" >> trios_all.csv
+    KANPIG_DENOVO=$(java CheckDeNovo ${INPUT_DIR}/${CHILD_ID}_kanpig_all_gtmatrix.txt)
+    F07_DENOVO=$(java CheckDeNovo ${INPUT_DIR}/${CHILD_ID}_07_all_gtmatrix.txt)
+    F09_DENOVO=$(java CheckDeNovo ${INPUT_DIR}/${CHILD_ID}_09_all_gtmatrix.txt)
+    COHORT_MERGED_07_DENOVO=$(java CheckDeNovo ${INPUT_DIR}/${CHILD_ID}_cohort_merged_07_all_gtmatrix.txt)
+    COHORT_MERGED_09_DENOVO=$(java CheckDeNovo ${INPUT_DIR}/${CHILD_ID}_cohort_merged_09_all_gtmatrix.txt)
+    COHORT_REGENOTYPED_07_DENOVO=$(java CheckDeNovo ${INPUT_DIR}/${CHILD_ID}_cohort_regenotyped_07_all_gtmatrix.txt)
+    COHORT_REGENOTYPED_09_DENOVO=$(java CheckDeNovo ${INPUT_DIR}/${CHILD_ID}_cohort_regenotyped_09_all_gtmatrix.txt)
+    
+    echo "${KANPIG_N_GOOD_ALT},${KANPIG_N_MERR},${F07_N_GOOD_ALT},${F07_N_MERR},${F09_N_GOOD_ALT},${F09_N_MERR},${COHORT_MERGED_07_N_GOOD_ALT},${COHORT_MERGED_07_N_MERR},${COHORT_MERGED_09_N_GOOD_ALT},${COHORT_MERGED_09_N_MERR},${COHORT_REGENOTYPED_07_N_GOOD_ALT},${COHORT_REGENOTYPED_07_N_MERR},${COHORT_REGENOTYPED_09_N_GOOD_ALT},${COHORT_REGENOTYPED_09_N_MERR},${KANPIG_DENOVO},${F07_DENOVO},${F09_DENOVO},${COHORT_MERGED_07_DENOVO},${COHORT_MERGED_09_DENOVO},${COHORT_REGENOTYPED_07_DENOVO},${COHORT_REGENOTYPED_09_DENOVO}" >> trios_all.csv
     
     # Inside TRs
     KANPIG_N_GOOD_ALT=$(grep ^ngood_alt ${INPUT_DIR}/${CHILD_ID}_kanpig_tr.txt | cut -f 2)
@@ -55,7 +63,15 @@ while read CHILD_ID; do
     COHORT_REGENOTYPED_09_N_GOOD_ALT=$(grep ^ngood_alt ${INPUT_DIR}/${CHILD_ID}_cohort_regenotyped_09_tr.txt | cut -f 2)
     COHORT_REGENOTYPED_09_N_MERR=$(grep ^nmerr ${INPUT_DIR}/${CHILD_ID}_cohort_regenotyped_09_tr.txt | cut -f 2)
     
-    echo "${KANPIG_N_GOOD_ALT},${KANPIG_N_MERR},${F07_N_GOOD_ALT},${F07_N_MERR},${F09_N_GOOD_ALT},${F09_N_MERR},${COHORT_MERGED_07_N_GOOD_ALT},${COHORT_MERGED_07_N_MERR},${COHORT_MERGED_09_N_GOOD_ALT},${COHORT_MERGED_09_N_MERR},${COHORT_REGENOTYPED_07_N_GOOD_ALT},${COHORT_REGENOTYPED_07_N_MERR},${COHORT_REGENOTYPED_09_N_GOOD_ALT},${COHORT_REGENOTYPED_09_N_MERR}" >> trios_tr.csv
+    KANPIG_DENOVO=$(java CheckDeNovo ${INPUT_DIR}/${CHILD_ID}_kanpig_tr_gtmatrix.txt)
+    F07_DENOVO=$(java CheckDeNovo ${INPUT_DIR}/${CHILD_ID}_07_tr_gtmatrix.txt)
+    F09_DENOVO=$(java CheckDeNovo ${INPUT_DIR}/${CHILD_ID}_09_tr_gtmatrix.txt)
+    COHORT_MERGED_07_DENOVO=$(java CheckDeNovo ${INPUT_DIR}/${CHILD_ID}_cohort_merged_07_tr_gtmatrix.txt)
+    COHORT_MERGED_09_DENOVO=$(java CheckDeNovo ${INPUT_DIR}/${CHILD_ID}_cohort_merged_09_tr_gtmatrix.txt)
+    COHORT_REGENOTYPED_07_DENOVO=$(java CheckDeNovo ${INPUT_DIR}/${CHILD_ID}_cohort_regenotyped_07_tr_gtmatrix.txt)
+    COHORT_REGENOTYPED_09_DENOVO=$(java CheckDeNovo ${INPUT_DIR}/${CHILD_ID}_cohort_regenotyped_09_tr_gtmatrix.txt)
+    
+    echo "${KANPIG_N_GOOD_ALT},${KANPIG_N_MERR},${F07_N_GOOD_ALT},${F07_N_MERR},${F09_N_GOOD_ALT},${F09_N_MERR},${COHORT_MERGED_07_N_GOOD_ALT},${COHORT_MERGED_07_N_MERR},${COHORT_MERGED_09_N_GOOD_ALT},${COHORT_MERGED_09_N_MERR},${COHORT_REGENOTYPED_07_N_GOOD_ALT},${COHORT_REGENOTYPED_07_N_MERR},${COHORT_REGENOTYPED_09_N_GOOD_ALT},${COHORT_REGENOTYPED_09_N_MERR},${KANPIG_DENOVO},${F07_DENOVO},${F09_DENOVO},${COHORT_MERGED_07_DENOVO},${COHORT_MERGED_09_DENOVO},${COHORT_REGENOTYPED_07_DENOVO},${COHORT_REGENOTYPED_09_DENOVO}" >> trios_tr.csv
     
     # Outside TRs
     KANPIG_N_GOOD_ALT=$(grep ^ngood_alt ${INPUT_DIR}/${CHILD_ID}_kanpig_not_tr.txt | cut -f 2)
@@ -79,6 +95,14 @@ while read CHILD_ID; do
     COHORT_REGENOTYPED_09_N_GOOD_ALT=$(grep ^ngood_alt ${INPUT_DIR}/${CHILD_ID}_cohort_regenotyped_09_not_tr.txt | cut -f 2)
     COHORT_REGENOTYPED_09_N_MERR=$(grep ^nmerr ${INPUT_DIR}/${CHILD_ID}_cohort_regenotyped_09_not_tr.txt | cut -f 2)
     
-    echo "${KANPIG_N_GOOD_ALT},${KANPIG_N_MERR},${F07_N_GOOD_ALT},${F07_N_MERR},${F09_N_GOOD_ALT},${F09_N_MERR},${COHORT_MERGED_07_N_GOOD_ALT},${COHORT_MERGED_07_N_MERR},${COHORT_MERGED_09_N_GOOD_ALT},${COHORT_MERGED_09_N_MERR},${COHORT_REGENOTYPED_07_N_GOOD_ALT},${COHORT_REGENOTYPED_07_N_MERR},${COHORT_REGENOTYPED_09_N_GOOD_ALT},${COHORT_REGENOTYPED_09_N_MERR}" >> trios_not_tr.csv    
+    KANPIG_DENOVO=$(java CheckDeNovo ${INPUT_DIR}/${CHILD_ID}_kanpig_not_tr_gtmatrix.txt)
+    F07_DENOVO=$(java CheckDeNovo ${INPUT_DIR}/${CHILD_ID}_07_not_tr_gtmatrix.txt)
+    F09_DENOVO=$(java CheckDeNovo ${INPUT_DIR}/${CHILD_ID}_09_not_tr_gtmatrix.txt)
+    COHORT_MERGED_07_DENOVO=$(java CheckDeNovo ${INPUT_DIR}/${CHILD_ID}_cohort_merged_07_not_tr_gtmatrix.txt)
+    COHORT_MERGED_09_DENOVO=$(java CheckDeNovo ${INPUT_DIR}/${CHILD_ID}_cohort_merged_09_not_tr_gtmatrix.txt)
+    COHORT_REGENOTYPED_07_DENOVO=$(java CheckDeNovo ${INPUT_DIR}/${CHILD_ID}_cohort_regenotyped_07_not_tr_gtmatrix.txt)
+    COHORT_REGENOTYPED_09_DENOVO=$(java CheckDeNovo ${INPUT_DIR}/${CHILD_ID}_cohort_regenotyped_09_not_tr_gtmatrix.txt)
+    
+    echo "${KANPIG_N_GOOD_ALT},${KANPIG_N_MERR},${F07_N_GOOD_ALT},${F07_N_MERR},${F09_N_GOOD_ALT},${F09_N_MERR},${COHORT_MERGED_07_N_GOOD_ALT},${COHORT_MERGED_07_N_MERR},${COHORT_MERGED_09_N_GOOD_ALT},${COHORT_MERGED_09_N_MERR},${COHORT_REGENOTYPED_07_N_GOOD_ALT},${COHORT_REGENOTYPED_07_N_MERR},${COHORT_REGENOTYPED_09_N_GOOD_ALT},${COHORT_REGENOTYPED_09_N_MERR},${KANPIG_DENOVO},${F07_DENOVO},${F09_DENOVO},${COHORT_MERGED_07_DENOVO},${COHORT_MERGED_09_DENOVO},${COHORT_REGENOTYPED_07_DENOVO},${COHORT_REGENOTYPED_09_DENOVO}" >> trios_not_tr.csv
 done < children.txt
 rm -f children.txt
