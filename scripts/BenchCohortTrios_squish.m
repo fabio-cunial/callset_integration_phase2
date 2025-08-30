b5dd5b92604fa6a048af20eaca1b54feb3e1d7e5
fcunial <fcunial@broadinstitute.org>
2025-08-30 10:15:44 -0400

    fixes

--- scripts/BenchCohortTrios_squish.m
+++ scripts/BenchCohortTrios_squish.m
@@ -1,55 +1,46 @@
 SOURCE_DIR='.';
-FONT_SIZE=16;
+FONT_SIZE=12;
 DELTA=0.4;
-%LABELS={'v1','--squish','--ab 0.20','--maxhom 5','--fpenalty 0.2','--fpenalty 0.05','--gpenalty 0.08','--fnmax 1','--fnmax 6'};
-LABELS={'V1','SQ','AB','MH','FP1','FP2','GP','FN1','FN2'};
+LABELS={'V1','ND1','ND2', 'ND3','HS1','HS2', 'HS3','HS4','SZ1', 'SZ2','SS1','SS2', 'MQ1','MQ2','MQ3', 'MQ4'};
+%LABELS={'V1','SZ1','SZ2','SS1','SS2','MQ1','MQ2','MQ3','MQ4'};
+N_ARGS=length(LABELS);
+N_TOTAL_ARGS=16;
+MAX_MENDELIAN_ERROR=0.3;
+MAX_DE_NOVO=0.16;
 
 % Columns:
-% ${COHORT_REGENOTYPED_07_N_GOOD_ALT},${COHORT_REGENOTYPED_07_N_MERR},
-% ${SQUISH_N_GOOD_ALT},${SQUISH_N_MERR},
-% ${AB_N_GOOD_ALT},${AB_N_MERR},
-% ${MAXHOM_N_GOOD_ALT},${MAXHOM_N_MERR},
-% ${FPENALTY1_N_GOOD_ALT},${FPENALTY1_N_MERR},
-% ${FPENALTY2_N_GOOD_ALT},${FPENALTY2_N_MERR},
-% ${GPENALTY_N_GOOD_ALT},${GPENALTY_N_MERR},
-% ${FNMAX1_N_GOOD_ALT},${FNMAX1_N_MERR},
-% ${FNMAX2_N_GOOD_ALT},${FNMAX2_N_MERR},
+% ${N_GOOD_ALT_i},${N_MERR_i},
+% ...,
 %
-% ${COHORT_REGENOTYPED_07_DENOVO},  // 19-th column (1-based)
-% ${SQUISH_DENOVO},
-% ${AB_DENOVO},
-% ${MAXHOM_DENOVO},
-% ${FPENALTY1_DENOVO},
-% ${FPENALTY2_DENOVO},
-% ${GPENALTY_DENOVO},
-% ${FNMAX1_DENOVO},
-% ${FNMAX2_DENOVO}
+% ${DENOVO_i},
+% ...
 
 
 figure(1);
 
 # All, 07.
 A=load(sprintf('%s/squish_trios_all.csv',SOURCE_DIR));
+
 [nrows,ncolumns]=size(A);
 
 subplot(2,3,1); hold on;
-for i=[1:9]
+for i=[1:N_ARGS]
     X=ones(nrows,1).*i -DELTA/2 + rand(nrows,1).*DELTA;
     Y=A(:,2*i)./(A(:,2*i)+A(:,2*i-1));
     plot(X,Y,'.');
 endfor
-title('All variants, 07.'); axis([0,10,0,0.15]); axis square; grid on; 
-xticks([1:9]); xticklabels(LABELS); xtickangle(45);
+title('All variants, 07.'); axis([0,N_ARGS+1,0,MAX_MENDELIAN_ERROR]); grid on; 
+xticks([1:N_ARGS]); xticklabels(LABELS);
 ylabel('Mendelian error rate'); set(gca,'fontsize',FONT_SIZE);
 
 subplot(2,3,4); hold on;
-for i=[19:27]
-    X=ones(nrows,1).*(i-18) -DELTA/2 + rand(nrows,1).*DELTA;
-    Y=A(:,i);
+for i=[1:N_ARGS]
+    X=ones(nrows,1).*i -DELTA/2 + rand(nrows,1).*DELTA;
+    Y=A(:,N_TOTAL_ARGS*2+i);
     plot(X,Y,'.');
 endfor
-title('All variants, 07.'); axis([0,10,0,0.15]); axis square; grid on; 
-xticks([1:9]); xticklabels(LABELS);
+title('All variants, 07.'); axis([0,N_ARGS+1,0,MAX_DE_NOVO]); grid on; 
+xticks([1:N_ARGS]); xticklabels(LABELS);
 ylabel('De novo rate'); set(gca,'fontsize',FONT_SIZE);
 
 
@@ -58,23 +49,23 @@ A=load(sprintf('%s/squish_trios_tr.csv',SOURCE_DIR));
 [nrows,ncolumns]=size(A);
 
 subplot(2,3,2); hold on;
-for i=[1:9]
+for i=[1:N_ARGS]
     X=ones(nrows,1).*i -DELTA/2 + rand(nrows,1).*DELTA;
     Y=A(:,2*i)./(A(:,2*i)+A(:,2*i-1));
     plot(X,Y,'.');
 endfor
-title('Inside TR, 07.'); axis([0,10,0,0.15]); axis square; grid on; 
-xticks([1:9]); xticklabels(LABELS); xtickangle(45);
+title('Inside TR, 07.'); axis([0,N_ARGS+1,0,MAX_MENDELIAN_ERROR]); grid on; 
+xticks([1:N_ARGS]); xticklabels(LABELS);
 ylabel('Mendelian error rate'); set(gca,'fontsize',FONT_SIZE);
 
 subplot(2,3,5); hold on;
-for i=[19:27]
-    X=ones(nrows,1).*(i-18) -DELTA/2 + rand(nrows,1).*DELTA;
-    Y=A(:,i);
+for i=[1:N_ARGS]
+    X=ones(nrows,1).*i -DELTA/2 + rand(nrows,1).*DELTA;
+    Y=A(:,N_TOTAL_ARGS*2+i);
     plot(X,Y,'.');
 endfor
-title('Inside TR, 07.'); axis([0,10,0,0.15]); axis square; grid on; 
-xticks([1:9]); xticklabels(LABELS);
+title('Inside TR, 07.'); axis([0,N_ARGS+1,0,MAX_DE_NOVO]); grid on; 
+xticks([1:N_ARGS]); xticklabels(LABELS);
 ylabel('De novo rate'); set(gca,'fontsize',FONT_SIZE);
 
 
@@ -83,21 +74,21 @@ A=load(sprintf('%s/squish_trios_not_tr.csv',SOURCE_DIR));
 [nrows,ncolumns]=size(A);
 
 subplot(2,3,3); hold on;
-for i=[1:9]
+for i=[1:N_ARGS]
     X=ones(nrows,1).*i -DELTA/2 + rand(nrows,1).*DELTA;
     Y=A(:,2*i)./(A(:,2*i)+A(:,2*i-1));
     plot(X,Y,'.');
 endfor
-title('Outside TR, 07.'); axis([0,10,0,0.15]); axis square; grid on; 
-xticks([1:9]); xticklabels(LABELS); xtickangle(45);
+title('Outside TR, 07.'); axis([0,N_ARGS+1,0,MAX_MENDELIAN_ERROR]); grid on; 
+xticks([1:N_ARGS]); xticklabels(LABELS);
 ylabel('Mendelian error rate'); set(gca,'fontsize',FONT_SIZE);
 
 subplot(2,3,6); hold on;
-for i=[19:27]
-    X=ones(nrows,1).*(i-18) -DELTA/2 + rand(nrows,1).*DELTA;
-    Y=A(:,i);
+for i=[1:N_ARGS]
+    X=ones(nrows,1).*i -DELTA/2 + rand(nrows,1).*DELTA;
+    Y=A(:,N_TOTAL_ARGS*2+i);
     plot(X,Y,'.');
 endfor
-title('Outside TR, 07.'); axis([0,10,0,0.15]); axis square; grid on; 
-xticks([1:9]); xticklabels(LABELS);
+title('Outside TR, 07.'); axis([0,N_ARGS+1,0,MAX_DE_NOVO]); grid on; 
+xticks([1:N_ARGS]); xticklabels(LABELS);
 ylabel('De novo rate'); set(gca,'fontsize',FONT_SIZE);
--- scripts/BenchCohortTrios_squish.sh
+++ scripts/BenchCohortTrios_squish.sh
@@ -2,6 +2,9 @@
 #
 TRIOS_TSV="/Users/fcunial/Downloads/trios_only_5.tsv"
 INPUT_DIR="/Users/fcunial/Downloads/BenchCohortTrios_squish"
+INPUT_DIR_NEW="/Users/fcunial/Downloads/BenchCohortTrios_squish/new_args"
+
+IDS="neighdist1 neighdist2 neighdist3 hapsim1 hapsim2 hapsim3 hapsim4 sizesim1 sizesim2 seqsim1 seqsim2 mapq1 mapq2 mapq3 mapq4"
 
 set -euxo pipefail
 
@@ -10,42 +13,21 @@ rm -f squish_trios_all.csv squish_trios_tr.csv squish_trios_not_tr.csv
 cut -f 2 ${TRIOS_TSV} > children.txt
 while read CHILD_ID; do
     for SUFFIX in all tr not_tr; do
-        COHORT_REGENOTYPED_07_N_GOOD_ALT=$(grep ^ngood_alt ${INPUT_DIR}/${CHILD_ID}_cohort_regenotyped_07_${SUFFIX}.txt | cut -f 2)
-        COHORT_REGENOTYPED_07_N_MERR=$(grep ^nmerr ${INPUT_DIR}/${CHILD_ID}_cohort_regenotyped_07_${SUFFIX}.txt | cut -f 2)
-    
-        SQUISH_N_GOOD_ALT=$(grep ^ngood_alt ${INPUT_DIR}/${CHILD_ID}_squish_${SUFFIX}.txt | cut -f 2)
-        SQUISH_N_MERR=$(grep ^nmerr ${INPUT_DIR}/${CHILD_ID}_squish_${SUFFIX}.txt | cut -f 2)
-    
-        AB_N_GOOD_ALT=$(grep ^ngood_alt ${INPUT_DIR}/${CHILD_ID}_ab_${SUFFIX}.txt | cut -f 2)
-        AB_N_MERR=$(grep ^nmerr ${INPUT_DIR}/${CHILD_ID}_ab_${SUFFIX}.txt | cut -f 2)
-    
-        MAXHOM_N_GOOD_ALT=$(grep ^ngood_alt ${INPUT_DIR}/${CHILD_ID}_maxhom_${SUFFIX}.txt | cut -f 2)
-        MAXHOM_N_MERR=$(grep ^nmerr ${INPUT_DIR}/${CHILD_ID}_maxhom_${SUFFIX}.txt | cut -f 2)
-    
-        FPENALTY1_N_GOOD_ALT=$(grep ^ngood_alt ${INPUT_DIR}/${CHILD_ID}_fpenalty1_${SUFFIX}.txt | cut -f 2)
-        FPENALTY1_N_MERR=$(grep ^nmerr ${INPUT_DIR}/${CHILD_ID}_fpenalty1_${SUFFIX}.txt | cut -f 2)
-        FPENALTY2_N_GOOD_ALT=$(grep ^ngood_alt ${INPUT_DIR}/${CHILD_ID}_fpenalty2_${SUFFIX}.txt | cut -f 2)
-        FPENALTY2_N_MERR=$(grep ^nmerr ${INPUT_DIR}/${CHILD_ID}_fpenalty2_${SUFFIX}.txt | cut -f 2)
-    
-        GPENALTY_N_GOOD_ALT=$(grep ^ngood_alt ${INPUT_DIR}/${CHILD_ID}_gpenalty_${SUFFIX}.txt | cut -f 2)
-        GPENALTY_N_MERR=$(grep ^nmerr ${INPUT_DIR}/${CHILD_ID}_gpenalty_${SUFFIX}.txt | cut -f 2)
-    
-        FNMAX1_N_GOOD_ALT=$(grep ^ngood_alt ${INPUT_DIR}/${CHILD_ID}_fnmax1_${SUFFIX}.txt | cut -f 2)
-        FNMAX1_N_MERR=$(grep ^nmerr ${INPUT_DIR}/${CHILD_ID}_fnmax1_${SUFFIX}.txt | cut -f 2)
-        FNMAX2_N_GOOD_ALT=$(grep ^ngood_alt ${INPUT_DIR}/${CHILD_ID}_fnmax2_${SUFFIX}.txt | cut -f 2)
-        FNMAX2_N_MERR=$(grep ^nmerr ${INPUT_DIR}/${CHILD_ID}_fnmax2_${SUFFIX}.txt | cut -f 2)
-    
-        COHORT_REGENOTYPED_07_DENOVO=$(java CheckDeNovo ${INPUT_DIR}/${CHILD_ID}_cohort_regenotyped_07_${SUFFIX}_gtmatrix.txt)
-        SQUISH_DENOVO=$(java CheckDeNovo ${INPUT_DIR}/${CHILD_ID}_squish_${SUFFIX}_gtmatrix.txt)
-        AB_DENOVO=$(java CheckDeNovo ${INPUT_DIR}/${CHILD_ID}_ab_${SUFFIX}_gtmatrix.txt)
-        MAXHOM_DENOVO=$(java CheckDeNovo ${INPUT_DIR}/${CHILD_ID}_maxhom_${SUFFIX}_gtmatrix.txt)
-        FPENALTY1_DENOVO=$(java CheckDeNovo ${INPUT_DIR}/${CHILD_ID}_fpenalty1_${SUFFIX}_gtmatrix.txt)
-        FPENALTY2_DENOVO=$(java CheckDeNovo ${INPUT_DIR}/${CHILD_ID}_fpenalty2_${SUFFIX}_gtmatrix.txt)
-        GPENALTY_DENOVO=$(java CheckDeNovo ${INPUT_DIR}/${CHILD_ID}_gpenalty_${SUFFIX}_gtmatrix.txt)
-        FNMAX1_DENOVO=$(java CheckDeNovo ${INPUT_DIR}/${CHILD_ID}_fnmax1_${SUFFIX}_gtmatrix.txt)
-        FNMAX2_DENOVO=$(java CheckDeNovo ${INPUT_DIR}/${CHILD_ID}_fnmax2_${SUFFIX}_gtmatrix.txt)
-    
-        echo "${COHORT_REGENOTYPED_07_N_GOOD_ALT},${COHORT_REGENOTYPED_07_N_MERR},${SQUISH_N_GOOD_ALT},${SQUISH_N_MERR},${AB_N_GOOD_ALT},${AB_N_MERR},${MAXHOM_N_GOOD_ALT},${MAXHOM_N_MERR},${FPENALTY1_N_GOOD_ALT},${FPENALTY1_N_MERR},${FPENALTY2_N_GOOD_ALT},${FPENALTY2_N_MERR},${GPENALTY_N_GOOD_ALT},${GPENALTY_N_MERR},${FNMAX1_N_GOOD_ALT},${FNMAX1_N_MERR},${FNMAX2_N_GOOD_ALT},${FNMAX2_N_MERR},${COHORT_REGENOTYPED_07_DENOVO},${SQUISH_DENOVO},${AB_DENOVO},${MAXHOM_DENOVO},${FPENALTY1_DENOVO},${FPENALTY2_DENOVO},${GPENALTY_DENOVO},${FNMAX1_DENOVO},${FNMAX2_DENOVO}" >> squish_trios_${SUFFIX}.csv
+        N_GOOD_ALT=$(grep ^ngood_alt ${INPUT_DIR}/${CHILD_ID}_cohort_regenotyped_07_${SUFFIX}.txt | cut -f 2)
+        N_MERR=$(grep ^nmerr ${INPUT_DIR}/${CHILD_ID}_cohort_regenotyped_07_${SUFFIX}.txt | cut -f 2)
+        ROW="${N_GOOD_ALT},${N_MERR}"
+        for ID in ${IDS}; do
+            N_GOOD_ALT=$(grep ^ngood_alt ${INPUT_DIR_NEW}/${CHILD_ID}_${ID}_${SUFFIX}.txt | cut -f 2)
+            N_MERR=$(grep ^nmerr ${INPUT_DIR_NEW}/${CHILD_ID}_${ID}_${SUFFIX}.txt | cut -f 2)
+            ROW="${ROW},${N_GOOD_ALT},${N_MERR}"
+        done
+        DENOVO=$(java CheckDeNovo ${INPUT_DIR}/${CHILD_ID}_cohort_regenotyped_07_${SUFFIX}_gtmatrix.txt)
+        ROW="${ROW},${DENOVO}"
+        for ID in ${IDS}; do
+            DENOVO=$(java CheckDeNovo ${INPUT_DIR_NEW}/${CHILD_ID}_${ID}_${SUFFIX}_gtmatrix.txt)
+            ROW="${ROW},${DENOVO}"
+        done
+        echo "${ROW}" >> squish_trios_${SUFFIX}.csv
     done
 done < children.txt
 rm -f children.txt
