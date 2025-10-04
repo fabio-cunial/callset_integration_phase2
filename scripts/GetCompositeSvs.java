3df904f4c3109daa5042b59428356502a1e3b309
fcunial <fcunial@broadinstitute.org>
2025-10-03 20:23:32 -0400

    fixes

--- scripts/GetCompositeSvs.java
+++ scripts/GetCompositeSvs.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+import java.util.*;
 import java.util.zip.GZIPInputStream;
 import java.io.*;
 
@@ -7,37 +7,49 @@ import java.io.*;
  * Uses the NE field printed by kanpig to build a BED of kanpig regions.
  */
 public class GetCompositeSvs {
+    /**
+     * Temporary space to store the calls that occur consecutively on the same 
+     * haplotype.
+     */
+    private static int[] haplotype;
+    private static int haplotypeLast;
+    
     
     /**
-     * 
-     *
-     * @param args 0 a VCF whose FORMAT field is:
-     *
+     * @param args 0 a sorted inter-sample VCF, whose FORMAT field is:
      * GT:FT:SQ:GQ:PS:NE:DP:AD:KS
-     *
      */
     public static void main(String[] args) throws IOException {
         final String COHORT_VCF_GZ = args[0];
+        final int MAX_DISTANCE = Integer.parseInt(args[1]);
+        final int MIN_CALLS = Integer.parseInt(args[2]);
         
-        final int QUANTUM = 10000;
+        final int CAPACITY = 10;  // Arbitrary
+        final int QUANTUM = 10000;  // Arbitrary
         
         int lastCall, nRecords, region, currentRegion;
         String str;
         BufferedReader br;
-        String[] tokens, tokensPrime;
+        String[] tokens, tokensPrime, columns;
         String[][] calls;
         
+        calls = new String[CAPACITY][0]; lastCall=-1;
+        columns=null;
         br = new BufferedReader(new InputStreamReader(new GZIPInputStream(new FileInputStream(COHORT_VCF_GZ))));
         str=br.readLine(); currentRegion=-1; nRecords=0;
         while (str!=null) { 
-            if (str.charAt(0)=='#') { str=br.readLine(); continue; }
+            if (str.charAt(0)=='#') { 
+                if (str.substring(0,6).equalsIgnoreCase("#CHROM")) columns=str.split("\t");
+                str=br.readLine();
+                continue; 
+            }
             nRecords++;
             if (nRecords%QUANTUM==0) System.err.println("Processed "+nRecords+" records...");
             tokens=str.split("\t");
             tokensPrime=tokens[9].split(":");
             region=Integer.parseInt(tokensPrime[5]);
             if (region!=currentRegion) {
-                if (currentRegion!=-1) getCompositeSvs(calls,lastCall);
+                if (currentRegion!=-1) getCompositeSvs(calls,lastCall,MAX_DISTANCE,MIN_CALLS,columns);
                 currentRegion=region; lastCall=0; calls[0]=tokens;
             }
             else {
@@ -50,14 +62,14 @@ public class GetCompositeSvs {
             }
             str=br.readLine();
         }
-        if (currentRegion!=-1) getCompositeSvs(calls,lastCall);
+        if (currentRegion!=-1) getCompositeSvs(calls,lastCall,MAX_DISTANCE,MIN_CALLS,columns);
         br.close();
     }
     
     
     /**
-     * @param maxDistance maximum distance between two calls to be considered
-     * part of the same composite event;
+     * @param maxDistance maximum distance between two calls that are present on
+     * the same haplotype to be considered part of the same composite event;
      * @param minCalls (>=2) min number of calls in a composite event.
      */
     private static final void getCompositeSvs(String[][] calls, int lastCall, int maxDistance, int minCalls, String[] columns) {
@@ -69,7 +81,7 @@ public class GetCompositeSvs {
         int i, j, k;
         int lastCompositeSv, callId;
         String key;
-        int[] haplotype, tmpArray;
+        int[] tmpArray;
         String[] tokens;
         int[][] compositeSvs;
         Vector<String> value;
@@ -77,69 +89,79 @@ public class GetCompositeSvs {
         Iterator<Map.Entry<String,Vector<String>>> iterator;
         Map.Entry<String,Vector<String>> entry;
         
+        // Building `compositeSvs` and `compositeSv2samples`.
+        compositeSvs = new int[lastCall+1][2];
         compositeSv2samples = new HashMap<String,Vector<String>>();
-        compositeSvs = new int[calls.length][2];
         haplotype = new int[CAPACITY];
         tmpArray = new int[2];
         for (j=10; j<N_COLUMNS; j++) {
-            loadHaplotype(calls,lastCall,false,IS_AUTOSOMAL);
-            getCompositeSVs(calls,maxDistance,minCalls,tmpArray);
+            loadHaplotype(j,false,IS_AUTOSOMAL,calls,lastCall);
             lastCompositeSv=getCompositeSvs(calls,maxDistance,minCalls,compositeSvs,tmpArray);
             for (k=0; k<=lastCompositeSv; k++) {
                 key=compositeSv2key(compositeSvs[k][0],compositeSvs[k][1]);
-                compositeSv2samples.put(key,columns[j]+"_0");
+                if (compositeSv2samples.containsKey(key)) {
+                    value=compositeSv2samples.get(key);
+                    value.add(columns[j]+"_0");
+                }
+                else {
+                    value = new Vector<String>();
+                    value.add(columns[j]+"_0");
+                    compositeSv2samples.put(key,value);
+                }
             }
             if (IS_AUTOSOMAL) {
-                loadHaplotype(calls,lastCall,true,true);
-                getCompositeSVs(calls,maxDistance,minCalls,tmpArray);
+                loadHaplotype(j,true,true,calls,lastCall);
                 lastCompositeSv=getCompositeSvs(calls,maxDistance,minCalls,compositeSvs,tmpArray);
                 for (k=0; k<=lastCompositeSv; k++) {
                     key=compositeSv2key(compositeSvs[k][0],compositeSvs[k][1]);
-                    compositeSv2samples.put(key,columns[j]+"_1");
+                    if (compositeSv2samples.containsKey(key)) {
+                        value=compositeSv2samples.get(key);
+                        value.add(columns[j]+"_1");
+                    }
+                    else {
+                        value = new Vector<String>();
+                        value.add(columns[j]+"_1");
+                        compositeSv2samples.put(key,value);
+                    }
                 }
             }
         }
         
-        // Outputting
+        // Printing `compositeSvs` and `compositeSv2samples`.
         iterator=compositeSv2samples.entrySet().iterator();
         while (iterator.hasNext()) {
             entry=iterator.next();
-            key=entry.getKey();
+            key=entry.getKey(); value=entry.getValue();
             tokens=key.split("-");
-            value=entry.getValue();
             System.out.println("Composite event:");
             for (i=0; i<tokens.length; i++) {
                 callId=Integer.parseInt(tokens[i]);
                 for (j=0; j<=7; j++) System.out.print(calls[callId][j]+"\t");
                 System.out.println();
             }
-            System.out.println("n_samples: "+value.size());
-            System.out.println("Samples:");
+            System.out.println("Occurs in "+value.size()+" samples:");
             for (i=0; i<value.size(); i++) System.out.print(value.elementAt(i)+",");
             System.out.println();
         }
     }
     
     
-    private static int[] haplotype;
-    private static int haplotypeLast;
-    
-    
     /**
-     * Remark: the procedure loads call IDs in global variable `haplotype`.
+     * The procedure loads call IDs (i.e. indexes in `calls`) in global variable
+     * `haplotype`.
      *
-     * @param which 0=left, 1=right.
+     * @param which 0=left hap, 1=right hap.
      */
-    private static final void loadHaplotype(String[][] calls, int lastCall, boolean which, boolean autosomal) {
-        int i;
+    private static final void loadHaplotype(int column, boolean which, boolean isAutosomal, String[][] calls, int lastCall) {
+        int i, j;
         
         haplotypeLast=-1;
         for (i=0; i<=lastCall; i++) {
-            if (autosomal && calls[i][j].charAt(1)!='|') {
+            if (isAutosomal && calls[i][column].charAt(1)!='|') {
                 // Skipping unphased calls
                 continue;
             }
-            if (calls[i][j].charAt(which?2:0)=='1') {
+            if (calls[i][column].charAt(which?2:0)=='1') {
                 haplotypeLast++;
                 if (haplotypeLast==haplotype.length) {
                     int[] newArray = new int[haplotype.length<<1];
@@ -153,10 +175,10 @@ public class GetCompositeSvs {
     
     
     /**
-     * Simply scans `haplotype` for blocks of >=`minCalls` consecutive calls at 
+     * Scans `haplotype` for blocks of >=`minCalls` consecutive calls at
      * distance <=`maxDistance` from each other, and stores `(i,j)` pairs in 
      * `out`, where `i` (resp. `j`) is the index of the first (resp. last) call 
-     * in a block.
+     * in a block (indexes are WRT `haplotype`).
      *
      * @param tmpArray temporary space with >=2 cells;
      * @return the last element in `out`.
@@ -166,10 +188,10 @@ public class GetCompositeSvs {
         int lastCompositeEvent, currentFirst, currentLast, currentStart, currentEnd;
         
         lastCompositeEvent=-1;
-        getInterval(calls,haplotype[0],tmpArray);
+        getInterval(haplotype[0],calls,tmpArray);
         currentFirst=0; currentLast=0; currentStart=tmpArray[0]; currentEnd=tmpArray[1];
         for (i=1; i<=haplotypeLast; i++) {
-            getInterval(calls,haplotype[i],tmpArray);
+            getInterval(haplotype[i],calls,tmpArray);
             if (tmpArray[0]-currentEnd>maxDistance) {
                 if (currentLast-currentFirst+1>=minCalls) {
                     lastCompositeEvent++;
@@ -190,40 +212,44 @@ public class GetCompositeSvs {
     
     
     /**
+     * @param id position in `calls`;
      * @param out [start,end], 1-based, inclusive.
      */
-    private static final void getInterval(String[][] calls, int id, int[] out) {
+    private static final void getInterval(int id, String[][] calls, int[] out) {
         final int POS = Integer.parseInt(calls[id][1]);
         final String REF = calls[id][3];
         final String ALT = calls[id][4];
         
         int start, end;
+        
         if (REF.length()==1 && ALT.length()>1) {
             // INS
-            start=pos;
-            end=pos+1;
+            start=POS;
+            end=POS+1;
         }
         else if (REF.length()>1 && ALT.length()==1) {
             // DEL
-            start=pos+1;
-            end=pos+ref.length();
+            start=POS+1;
+            end=POS+REF.length();
         }
         else {
-            start=pos+1;
-            end=pos+ref.length();
+            start=POS+1;
+            end=POS+REF.length();
         }
         out[0]=start; out[1]=end;
     }
     
     
+    /**
+     * @para, first,last positions in `haplotype`.
+     */
     private static final String compositeSv2key(int first, int last) {
         int i;
         String out;
         
-        out=haplotype[first];
+        out=haplotype[first]+"";
         for (i=first+1; i<=last; i++) out+="-"+haplotype[i];
         return out;
     }
-    
     
 }
