0f03f2673f7c93f2d75088f6890af0b55b8355da
fcunial <fcunial@broadinstitute.org>
2025-10-15 16:10:58 -0400

    fixes

--- wdl/validateWdl.sh
+++ wdl/validateWdl.sh
@@ -3,6 +3,7 @@
 set -x
 WOMTOOL_PATH="/Users/fcunial/apps/cromwell/womtool-84.jar"
 
+java -jar ${WOMTOOL_PATH} validate -l GetCompositeSvs.wdl
 java -jar ${WOMTOOL_PATH} validate -l Bam2Fastq.wdl
 java -jar ${WOMTOOL_PATH} validate -l GetNCalls2.wdl
 java -jar ${WOMTOOL_PATH} validate -l GetNCalls.wdl
