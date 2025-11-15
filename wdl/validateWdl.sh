a5f1d3b2cbb64f634bba0f8f1607060f03f18129
fcunial <fcunial@broadinstitute.org>
2025-11-15 18:40:37 -0500

    fixes

--- wdl/validateWdl.sh
+++ wdl/validateWdl.sh
@@ -3,6 +3,7 @@
 set -x
 WOMTOOL_PATH="/Users/fcunial/apps/cromwell/womtool-84.jar"
 
+java -jar ${WOMTOOL_PATH} validate -l RegenotypeKanpigHapsVcf.wdl
 java -jar ${WOMTOOL_PATH} validate -l BuildKanpigHapVcf.wdl
 java -jar ${WOMTOOL_PATH} validate -l PhabRegenotypedCohort.wdl
 java -jar ${WOMTOOL_PATH} validate -l SV_Integration_RegenotypingAnalysis.wdl
