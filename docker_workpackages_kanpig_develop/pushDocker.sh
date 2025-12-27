6ca6049f0397e4033c795c9979b13143eff7a168
fcunial <fcunial@broadinstitute.org>
2025-12-27 14:22:11 -0500

    fixes

---
+++ docker_workpackages_kanpig_develop/pushDocker.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+set -euxo pipefail
+
+if [ $# -eq 0 ]; then
+  echo "Need to provide tag for the docker image."
+  exit 1
+fi
+
+TAG="$1"
+
+cp ../scripts/*.java .
+cp ../scripts/*.r .
+docker build --progress=plain -t fcunial/kanpig_develop .
+docker tag fcunial/kanpig_develop fcunial/kanpig_develop:${TAG}
+docker push fcunial/kanpig_develop:${TAG}
+rm -f *.java *.class *.r
